2c1d8cd50d044318933ebaf89022f47f0e5fafb3
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2024-01-17 19:24:13 -0600

    .

---
+++ Cubo.py
@@ -0,0 +1,125 @@
+#Autor: Ivan Olmos Pineda
+
+
+import pygame
+from pygame.locals import *
+
+# Cargamos las bibliotecas de OpenGL
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+import random
+import math
+import numpy as np
+
+class Cubo:
+    
+    def __init__(self, dim, vel, scale):
+        self.scale = scale
+        self.radius = math.sqrt(3) 
+        self.isCollided = False
+        
+        #vertices del cubo
+        self.points = np.array([[-1.0,-1.0, 1.0], [1.0,-1.0, 1.0], [1.0,-1.0,-1.0], [-1.0,-1.0,-1.0],
+                                [-1.0, 1.0, 1.0], [1.0, 1.0, 1.0], [1.0, 1.0,-1.0], [-1.0, 1.0,-1.0]])
+        
+        self.DimBoard = dim
+        #Se inicializa una posicion aleatoria en el tablero
+        self.Position = []
+        self.Position.append(random.randint(-1 * self.DimBoard, self.DimBoard))
+        self.Position.append(5.0)
+        self.Position.append(random.randint(-1 * self.DimBoard, self.DimBoard))
+        #Se inicializa un vector de direccion aleatorio
+        self.Direction = []
+        self.Direction.append(random.random())
+        self.Direction.append(5.0)
+        self.Direction.append(random.random())
+        #Se normaliza el vector de direccion
+        m = math.sqrt(self.Direction[0]*self.Direction[0] + self.Direction[2]*self.Direction[2])
+        self.Direction[0] /= m
+        self.Direction[2] /= m
+        #Se cambia la maginitud del vector direccion
+        self.Direction[0] *= vel
+        self.Direction[2] *= vel        
+
+    def update(self):
+        if (self.isCollided): return
+        new_x = self.Position[0] + self.Direction[0]
+        new_z = self.Position[2] + self.Direction[2]
+        
+        #detecc de que el objeto no se salga del area de navegacion
+        if(abs(new_x) <= self.DimBoard):
+            self.Position[0] = new_x
+        else:
+            self.Direction[0] *= -1.0
+            self.Position[0] += self.Direction[0]
+        
+        if(abs(new_z) <= self.DimBoard):
+            self.Position[2] = new_z
+        else:
+            self.Direction[2] *= -1.0
+            self.Position[2] += self.Direction[2] 
+            
+    def collision(self, listCubes):        
+        nextPosition = [self.Position[0] + self.Direction[0], self.Position[1] + self.Direction[1], self.Position[2] + self.Direction[2]]
+        
+        for cube in listCubes:
+            if (cube == self): return
+            
+            nextCubePos = [cube.Position[0] + cube.Direction[0], cube.Position[1] + cube.Direction[1], cube.Position[2] + cube.Direction[2]]
+            euclideanDistance = math.sqrt(math.pow(nextCubePos[0] - nextPosition[0], 2) + 
+                                          math.pow(nextCubePos[2] - nextPosition[2], 2))
+                        
+            if (euclideanDistance <= cube.radius + self.radius):
+                self.isCollided = True
+                self.Direction = [0,0,0]
+                cube.isCollided = True
+                cube.Direction = [0,0,0]
+                break
+
+    def drawFaces(self):
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[0])
+        glVertex3fv(self.points[1])
+        glVertex3fv(self.points[2])
+        glVertex3fv(self.points[3])
+        glEnd()
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[4])
+        glVertex3fv(self.points[5])
+        glVertex3fv(self.points[6])
+        glVertex3fv(self.points[7])
+        glEnd()
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[0])
+        glVertex3fv(self.points[1])
+        glVertex3fv(self.points[5])
+        glVertex3fv(self.points[4])
+        glEnd()
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[1])
+        glVertex3fv(self.points[2])
+        glVertex3fv(self.points[6])
+        glVertex3fv(self.points[5])
+        glEnd()
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[2])
+        glVertex3fv(self.points[3])
+        glVertex3fv(self.points[7])
+        glVertex3fv(self.points[6])
+        glEnd()
+        glBegin(GL_QUADS)
+        glVertex3fv(self.points[3])
+        glVertex3fv(self.points[0])
+        glVertex3fv(self.points[4])
+        glVertex3fv(self.points[7])
+        glEnd()
+    
+    def draw(self):
+        glPushMatrix()
+        glTranslatef(self.Position[0], self.Position[1], self.Position[2])
+        glScaled(self.scale[0], self.scale[1], self.scale[2])
+        glColor3f(1.0, 1.0, 1.0)
+        self.drawFaces()
+        glPopMatrix()
---
+++ Plano.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+import random
+import math
+import numpy as np
